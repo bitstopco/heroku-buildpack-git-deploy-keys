451403e895ae09d669df0b8f75655bf48ff7c091
Harry Brundage <harry.brundage@gmail.com>
2017-04-18 13:51:37 -0400

    Add a basic test for ssh key dropping

---
+++ test/ssh_keys_test.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+. ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+
+afterSetUp()
+{
+  cp  ${BUILDPACK_HOME}/test/id_rsa ${ENV_DIR}/GITHUB_DEPLOY_KEY
+}
+
+testSSHKeysDroppedOnDisk()
+{
+  compile
+  assertCapturedSuccess
+  assertFileMD5 "b8e9a7bae8195a8812108a0a053e2918" ~/.ssh/id_rsa
+  assertTrue "[ -e ~/.ssh/known_hosts ]"
+}
+
+testssh_agentStartScriptAdded()
+{
+  compile
+  assertCapturedSuccess
+  cat ${STD_OUT}
+  assertTrue "[ -e ${BUILD_DIR}/.profile.d/launch_ssh_agent.sh ]"
+}
